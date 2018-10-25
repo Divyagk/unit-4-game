56630a6c4bb92659ea98e417bfe8665fac62adad
divya <gopalakrishnandivya6@gmail.com>
2018-10-25 11:43:21 -0600

    crystal game 2nd commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+//  speudo coding 
+//  win, loss,you win,youloss,randomnumber display(19-120),4crystal buttons(1-12)
+// user score display 
+//  Every crystal need to have a random #between 1-12 
+//  after every game we want to reset the crystal values 
+//  when clicking any crystal, it should adding with the previous result until the game win or loss 
+//  if the random number mathches the user score, the player win the game if not they loss 
+//  the value of the crystal should be hidded from the player 
+
+var randomResult;
+var lost;
+var win;
+
+// for(var i=0;i<4;i++){
+//     var crystal= $("<div>");
+//     crystal.attr("class",'crystal')
+//     $(".crystals").append(crystal);
+// }
