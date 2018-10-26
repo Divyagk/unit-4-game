70cd12fa0fdc80896ff2d584c408b158ba32a661
divya <gopalakrishnandivya6@gmail.com>
2018-10-26 11:01:59 -0600

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,37 @@
+
+var randomResult;
+var lost;
+var win;
+var Crystalnew;
+randomResult=Math.floor(Math.random()*101)+19;
+// console.log(randomResult)
+$("#randomresult-text").text( randomResult);
+for(var i=0;i<4;i++){
+    var random= Math.floor(Math.random()*11)+1;
+    // console.log(random)
+    var Crystalnew= $("<div>");
+    Crystalnew.attr({
+        "class":'Crystals',
+        "data-random": random
+})
+    $("Crystals").append(Crystalnew);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //  speudo coding 
 //  win, loss,you win,youloss,randomnumber display(19-120),4crystal buttons(1-12)
 // user score display 
@@ -6,13 +40,3 @@
 //  when clicking any crystal, it should adding with the previous result until the game win or loss 
 //  if the random number mathches the user score, the player win the game if not they loss 
 //  the value of the crystal should be hidded from the player 
-
-var randomResult;
-var lost;
-var win;
-
-// for(var i=0;i<4;i++){
-//     var crystal= $("<div>");
-//     crystal.attr("class",'crystal')
-//     $(".crystals").append(crystal);
-// }
