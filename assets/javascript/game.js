24a50992531b52782de422b1a35e58160f74ab4f
divya <gopalakrishnandivya6@gmail.com>
2018-10-26 17:19:47 -0600

    modified

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,114 @@
 
 var randomResult;
-var lost;
-var win;
-var Crystalnew;
-randomResult=Math.floor(Math.random()*101)+19;
-// console.log(randomResult)
-$("#randomresult-text").text( randomResult);
-for(var i=0;i<4;i++){
-    var random= Math.floor(Math.random()*11)+1;
-    // console.log(random)
-    var Crystalnew= $("<div>");
-    Crystalnew.attr({
-        "class":'Crystals',
-        "data-random": random
-})
-    $("Crystals").append(Crystalnew);
+var lost = 0;
+var win = 0;
+var totalscore=0;
+// var Crystalnew;
+// var images=["./assets/images/image 1.jpg","./assets/images/image2.jpg"]
+// var keySound = new Audio('./assets/sounds/typewriter-key.wav');
+var winSound = new Audio('./assets/sounds/you-win.wav');
+var loseSound = new Audio('./assets/sounds/you-lose.wav');
+//  To generating random results.
+$(document).ready(function () {
+    randomResult = Math.floor(Math.random() * 101) + 19;
+    $("#randomresult-text").html(randomResult);
+
+// try
+var num1= Math.floor(Math.random()*11+1)
+  var num2= Math.floor(Math.random()*11+1)
+  var num3= Math.floor(Math.random()*11+1)
+  var num4= Math.floor(Math.random()*11+1)
+  
+//   function updateDisplay() { 
+  $('#wins-text').text(win);
+$('#Loss-text').text(lost);
+
+//resets the game
+function reset(){
+      
+      num1= Math.floor(Math.random()*11+1);
+      num2= Math.floor(Math.random()*11+1);
+      num3= Math.floor(Math.random()*11+1);
+      num4= Math.floor(Math.random()*11+1);
+      
+      
+      $('#totalscore-text').text(totalscore);
+    //   updateDisplay();
+      } 
+    //   function updateDisplay() { 
+    //     $('#wins-text').text(win);
+    //   $('#Loss-text').text(lost);}
+//adds the wins to the userTotal
+function yay(){
+   
+alert("You won!");
+winSound.play();
+  win++; 
+ 
+  $('#wins-text').text(win);
+  reset();
 }
+//addes the losses to the userTotal
+function loser(){
+   
+alert ("You lose!");
+loseSound.play();
+
+  lost++;
+  $('#Loss-text').text(lost);
+  reset()
+}
+//sets up click for jewels
+  $('#one').on ('click', function(){
+    totalscore = totalscore + num1;
+    console.log("New userTotal= " + totalscore);
+    $('#totalscore-text').text(totalscore); 
+          //sets win/lose conditions
+        if (totalscore == randomResult){
+          yay();
+        }
+        else if (  totalscore> randomResult){
+          loser();
+        }   
+  })  
+  $('#two').on ('click', function(){
+    
+    totalscore = totalscore + num2;
+    console.log("New userTotal= " + totalscore);
+    $('#totalscore-text').text(totalscore); 
+        if (totalscore == randomResult){
+          yay();
+        }
+        else if ( totalscore > randomResult){
+          loser();
+        } 
+  })  
+  $('#three').on ('click', function(){
+    totalscore = totalscore + num3;
+    console.log("New userTotal= " + totalscore);
+    $('#totalscore-text').text(totalscore);
+//sets win/lose conditions
+          if (totalscore == randomResult){
+          yay();
+        }
+        else if ( totalscore > randomResult){
+          loser();
+        } 
+  })  
+  $('#four').on ('click', function(){
+    totalscore = totalscore + num4;
+    console.log("New userTotal= " + totalscore);
+    $('#totalscore-text').text(totalscore); 
+      
+          if (totalscore == randomResult){
+          yay();
+        }
+        else if ( totalscore > randomResult){
+          loser();
+        }
+  });   
+});
+
 
 
 
